c3914fdef8b11f554f76ff00105be00d8673266d
c-brenn <brennc27@tcd.ie>
2015-11-12 14:30:25 +0000

    production env variables

---
+++ config/prod.secret.exs
@@ -0,0 +1,12 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+config :cs_todo, CsTodo.Endpoint,
+  secret_key_base: {:system, "SECRET_KEY_BASE"}
+
+# Configure your database
+config :cs_todo, CsTodo.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: {:system, "DATABASE_URL"}
